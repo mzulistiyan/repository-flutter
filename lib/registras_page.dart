c8fd85e446191a6d12c609529417a4ed7109e5ad
izulistiyan@gmail.com <izulistiyan@gmail.com>
2023-01-18 15:36:05 +0700

    Fitur Registrasi

---
+++ lib/registras_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class RegistrasiPage extends StatefulWidget {
+  const RegistrasiPage({super.key});
+
+  @override
+  State<RegistrasiPage> createState() => _RegistrasiPageState();
+}
+
+class _RegistrasiPageState extends State<RegistrasiPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
